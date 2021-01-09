f0467ebebcfa25a97ff652738dcd3ca4089e91f8
deric0201 <74003143+deric0201@users.noreply.github.com>
2021-01-09 11:56:29 +0800

    Add files via upload

---
+++ 0109-1.py
@@ -0,0 +1,19 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+x,y,z = mc.player.getTilePos()
+import random
+for i in range(1,21):
+    r = random.randrange(1,5)
+    
+    if r == 1:
+        mc.setBlocks(x,y,z,x,y,z+4,35)
+        z = z+4
+    if r == 2:
+        mc.setBlocks(x,y,z,x,y,z-4,57)
+        z = z-4
+    if r == 3:        
+        mc.setBlocks(x,y,z,x-4,y,z,49)
+        x = x-4
+    if r == 4:
+        mc.setBlocks(x,y,z,x+4,y,z,46)
+        x = x+4
